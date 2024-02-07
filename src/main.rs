6f683d66dbc35a1a8b92fa39f976e987f3c0dc70
Simon <1simonsteven@gmail.com>
2024-02-07 06:53:23 +0200

    The webserver actually works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("localhost:8080").expect("Failed to bind to address");
@@ -22,7 +22,22 @@ fn handle_client(mut stream: TcpStream) -> io::Result<()> {
     let mut buffer: [u8; 1024] = [0; 1024];
     let bytes_read = stream.read(& mut buffer).expect("Failed to read from stream");
     let received_text = String::from_utf8_lossy(&buffer[..bytes_read]);
-    println!("Received: {}", received_text);
 
+    // println!("Received: {}", received_text);
+    
+    let response = handle_http_request(&received_text);
+    stream.write(response.as_bytes()).expect("Failed to write to stream");
     Ok(())
+}
+
+fn handle_http_request(request: &str) -> String {
+    let parts: Vec<&str> = request.split("\r\n").collect();
+    let first_line_parts: Vec<&str> = parts[0].split(" ").collect();
+    let method = first_line_parts[0];
+    let path = first_line_parts[1];
+    let http_version = first_line_parts[2];
+    println!("Method: {}, Path: {}, HTTP Version: {}", method, path, http_version);
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", request);
+    response
 }
